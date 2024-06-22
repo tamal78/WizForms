7a6d0f003a723375d04bc166c98cf6d0a75c3c34
Tamal <tamalkundu2002@gmail.com>
2024-06-22 18:29:24 +0530

    branding

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,30 @@
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
 
-async function Logo() {
+const headingFont = localFont({
+  src: "../public/font.woff2",
+});
+
+export default function Logo() {
   const { userId }: { userId: string | null } = auth();
   return (
-    <Link
-      href={userId ? "/dashboard" : "/"}
-      className="font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer"
-    >
-      WizForms
+    <Link href={userId ? "/dashboard" : "/"}>
+      <div className="items-center gap-x-2 transition hover:opacity-75 md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <div
+          className={cn(
+            "font-bold text-3xl bg-gradient-to-r from-indigo-400 to-cyan-400 text-transparent bg-clip-text hover:cursor-pointer",
+            headingFont.className,
+            "hidden md:block",
+          )}
+        >
+          WizForms
+        </div>
+      </div>
     </Link>
   );
 }
-
-export default Logo;
