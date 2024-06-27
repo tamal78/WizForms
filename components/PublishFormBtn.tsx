1542d8eac5603b7641abe7bb14b3be397ea642f3
Tamal <tamalkundu2002@gmail.com>
2024-06-27 18:39:08 +0530

    fixed publish

--- components/PublishFormBtn.tsx
+++ components/PublishFormBtn.tsx
@@ -1,4 +1,4 @@
-import { PublishForm } from "@/actions/form";
+import { PublishForm, UpdateFormContent } from "@/actions/form";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { FaSpinner } from "react-icons/fa";
@@ -16,13 +16,17 @@ import {
 } from "./ui/alert-dialog";
 import { Button } from "./ui/button";
 import { toast } from "./ui/use-toast";
+import useDesigner from "./hooks/useDesigner";
 
 function PublishFormBtn({ id }: { id: number }) {
   const [loading, startTransition] = useTransition();
+  const { elements } = useDesigner();
   const router = useRouter();
 
   async function publishForm() {
     try {
+      const jsonElements = JSON.stringify(elements);
+      await UpdateFormContent(id, jsonElements);
       await PublishForm(id);
       toast({
         title: "Success",
